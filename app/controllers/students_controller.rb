96ab3d0aabe654446d2ed509469197717b909343
Sharnell Grant <sharnellgrant@gmail.com>
2020-11-30 15:27:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
  
  
  def create 
-   Student.create(first_name: params[:student][:first_name])
+   Student.create(first_name: params[:student][:first_name] ,last_name: params[:student][:last_name])
+   
  end 
 end
